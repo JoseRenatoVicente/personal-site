ca160177698785c451c6170d95abad9b841be456
Jose Vicente <contato@renatovicente.dev>
2025-07-15 20:53:53 -0300

    test

---
+++ additional-env.d.ts
@@ -0,0 +1,10 @@
+// Arquivo de declaração adicional para garantir que as tipagens sejam reconhecidas
+
+// Tipagens explícitas para React
+/// <reference types="react" />
+/// <reference types="react-dom" />
+
+// Tipagens explícitas para Node
+/// <reference types="node" />
+
+// Adicione aqui quaisquer outras tipagens necessárias
